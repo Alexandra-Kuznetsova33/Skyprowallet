4c5458f6cd1edae184aef6d0060edb69f576386c
Alexandra Kuznetsova <kuznetzovaschura@yandex.ru>
2026-08-05 18:32:50 +0300

    BrowserRouter с basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import './App.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Skyprowallet"
+  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <ExpensesProvider>
           <App />
